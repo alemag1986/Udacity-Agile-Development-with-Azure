a287c4677657fba3a3cbca49ba0ef5e05ea264a3
Alejandro Magnani <alejandro.magnani@gmail.com>
2020-10-07 14:19:14 -0500

    test file updated

--- test_app.py
+++ test_app.py
@@ -1,18 +1,6 @@
-import pytest
 from app import app
 
-@pytest.fixture
-def client():
-    app.app.config['TESTING'] = True
-    with app.app.test_client() as client:
-        yield client
-
-
-### Run to see failed test
-#def test_hello_add():
-#    assert add(test_hello_add.x) == 12
-
-def test_route_home():
-    rv = client.get('/')
-    print(rv.data)
-    assert b'No entries here so far' in rv.data
+def test_home():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'<h3>Sklearn Prediction Home</h3>'
